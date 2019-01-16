5e26e519ce665cbf42a60bc449272198156708ee
annagoldberg <agoldberg114@gmail.com>
2019-01-16 15:29:31 -0600

    more changes

--- app.js
+++ app.js
@@ -15,7 +15,33 @@ function myFunction() {
   }
 }
 
+function myFunction() {
+  var dots = document.getElementById("dash");
+  var moreText = document.getElementById("lessButtonTwo");
+  var btnText = document.getElementById("myButtonTwo");
 
+  if (dash.style.display === "none") {
+    dash.style.display = "inline";
+    btnText.innerHTML = "Read more > "; 
+    moreText.style.display = "none";
+  } else {
+    dash.style.display = "none";
+    btnText.innerHTML = "Read less < "; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+// var articles = document.querySelectorAll('article');
+
+// for(var i = 0; i < articles.length; i++) {
+//   (function(){
+//      var button = articles[i].querySelector('button');
+
+//      button.on('click', function() {
+//      });
+// });
+// }();
 
 
 // var button = ('readMore');
